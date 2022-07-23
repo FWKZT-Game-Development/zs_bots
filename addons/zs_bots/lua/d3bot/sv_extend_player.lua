38788f9370c70a39a80cf22d114c8abc28341bf7
Mka0207 <7522605+Mka0207@users.noreply.github.com>
2022-07-23 17:05:40 -0400

    Revert Vis fix

--- addons/zs_bots/lua/d3bot/sv_extend_player.lua
+++ addons/zs_bots/lua/d3bot/sv_extend_player.lua
@@ -295,11 +295,11 @@ function meta:D3bot_CanSeeTarget( fraction, target )
 	if not attackPos then return false end
 	local mem = self.D3bot_Mem
 	if mem and mem.TgtNodeOrNil and mem.NodeOrNil ~= mem.TgtNodeOrNil and mem.TgtNodeOrNil:GetMetaData().Params.See == "Disabled" then return false end
-	--[[local tr = D3bot.BotSeeTr
+	local tr = D3bot.BotSeeTr
 	tr.start = self:D3bot_GetViewCenter()
 	tr.endpos = attackPos
-	tr.filter = player.GetAll()]]
-	return attackPos and self:VisibleVec(attackPos) -- util.TraceHull( tr ).Hit
+	tr.filter = player.GetAll()
+	return attackPos and --[[self:VisibleVec(attackPos)]] util.TraceHull( tr ).Hit
 end
 
 function meta:D3bot_UpdateAngsOffshoot( angOffshoot )
