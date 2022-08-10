5014ba458a17ac4102b3c746725ae8760759c875
Mka0207 <7522605+Mka0207@users.noreply.github.com>
2022-08-09 22:49:56 -0400

    Small optimization for CanBeTgt.

--- addons/zs_bots/lua/d3bot/sv_zs_bot_handler/handlers/undead_fallback.lua
+++ addons/zs_bots/lua/d3bot/sv_zs_bot_handler/handlers/undead_fallback.lua
@@ -213,7 +213,7 @@ local potTargetEntClasses = {}  --"prop_*turret", "prop_arsenalcrate", "prop_man
 local potEntTargets = nil
 function HANDLER.CanBeTgt(bot, target)
 	if not target or not IsValid(target) then return end
-	if IsValid(target) and target:IsPlayer() and target ~= bot and target:Team() ~= TEAM_UNDEAD and target:GetObserverMode() == OBS_MODE_NONE and not target:IsFlagSet(FL_NOTARGET) and target:Alive() then return true end
+	if not target:IsValidLivingHuman() then return true end
 	if potEntTargets and table.HasValue(potEntTargets, target) then return true end
 end
 
