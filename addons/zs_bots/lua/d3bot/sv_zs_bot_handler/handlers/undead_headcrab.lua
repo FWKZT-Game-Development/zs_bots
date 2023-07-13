55d11bee37126a5f83dbd309c159e96e9fed65d7
Mka0207 <g4.tyler@live.com>
2023-07-13 08:29:19 -0400

    Bot headcrabs shouldn't target manhacks / drones.

--- addons/zs_bots/lua/d3bot/sv_zs_bot_handler/handlers/undead_headcrab.lua
+++ addons/zs_bots/lua/d3bot/sv_zs_bot_handler/handlers/undead_headcrab.lua
@@ -135,7 +135,7 @@ end
 -- Custom functions and settings --
 -----------------------------------
 
-local potTargetEntClasses = {"prop_*turret", "prop_arsenalcrate", "prop_manhack*"}
+local potTargetEntClasses = {}
 local potEntTargets = nil
 function HANDLER.CanBeTgt(bot, target)
 	if not target or not IsValid(target) then return end
