bccf55eb11d638fe61568ec928772a3b840ecc83
Bagellll <djmiller03@twcny.rr.com>
2023-10-11 03:31:29 -0400

    Fix bot target acquisition

--- addons/zs_bots/lua/d3bot/sv_zs_bot_handler/handlers/undead_headcrab.lua
+++ addons/zs_bots/lua/d3bot/sv_zs_bot_handler/handlers/undead_headcrab.lua
@@ -147,15 +147,13 @@ end
 
 function HANDLER.RerollTarget(bot)
 	-- Get humans or non zombie players or any players in this order
-	local players = D3bot.RemoveObsDeadTgts(team.GetPlayers(TEAM_HUMAN))
-	if #players == 0 and TEAM_UNDEAD then
+	--local players = D3bot.RemoveObsDeadTgts(GAMEMODE.HumanPlayers)
+	--if #players == 0 and TEAM_UNDEAD then
+		--players = D3bot.RemoveObsDeadTgts(player.GetAll())
+		--players = D3bot.From(players):Where(function(k, v) return v:Team() ~= TEAM_UNDEAD end).R
+	--end
+	--[[if #players == 0 then
 		players = D3bot.RemoveObsDeadTgts(player.GetAll())
-		players = D3bot.From(players):Where(function(k, v) return v:Team() ~= TEAM_UNDEAD end).R
-	end
-	if #players == 0 then
-		players = D3bot.RemoveObsDeadTgts(player.GetAll())
-	end
-	potEntTargets = D3bot.GetEntsOfClss(potTargetEntClasses)
-	local potTargets = table.Add(players, potEntTargets)
-	bot:D3bot_SetTgtOrNil(table.Random(potTargets), false, nil)
+	end]]
+	bot:D3bot_SetTgtOrNil(table.Random(potD3bot.RemoveObsDeadTgts(GAMEMODE.HumanPlayers)Targets), false, nil)
 end
